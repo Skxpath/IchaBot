7ce2e72b93052591d737b319b4ca130de66f72e0
Alex Zhang <cwzhang@sfu.ca>
2017-10-01 19:09:45 -0700

    Made basic boilerplate for connecting to server, logging messages, and a simple command

--- bot.js
+++ bot.js
@@ -1,1 +1,21 @@
 const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+const prefix = "`";
+
+const userToken = "MzY0MjE2MTIyNDgxODM2MDMy.DLMiQw.EMmJ2TxID3xw9-VcqgrzQnWoPhw";
+
+bot.login(userToken);
+
+bot.on("ready", function(){
+  console.log(`Ready as: ${bot.user.tag}`)
+})
+
+bot.on("message", function(msg){
+
+  if(msg.content == prefix + "ping") {
+    msg.channel.send("Ping command used!");
+  }
+
+  console.log(`[${msg.createdAt}] by ${msg.author.tag} on ${msg.guild} in #${msg.channel.name}: ${msg.content}`)
+})
