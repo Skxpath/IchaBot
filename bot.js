9896b7cd9035abec7bf1a13431db667265392002
Alex Zhang <cwzhang@sfu.ca>
2017-10-01 19:55:33 -0700

    Made config.json file and additional functionality

--- bot.js
+++ bot.js
@@ -1,21 +1,43 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-const prefix = "`";
+const fs = require("fs");
+const config = JSON.parse(fs.readFileSync("./configuration/config.json", "utf8"));
 
-const userToken = "MzY0MjE2MTIyNDgxODM2MDMy.DLMiQw.EMmJ2TxID3xw9-VcqgrzQnWoPhw";
 
-bot.login(userToken);
 
-bot.on("ready", function(){
+bot.login(config.token);
+
+bot.on("ready", function() {
   console.log(`Ready as: ${bot.user.tag}`)
 })
 
-bot.on("message", function(msg){
+bot.on("message", function(msg) {
 
-  if(msg.content == prefix + "ping") {
-    msg.channel.send("Ping command used!");
+var command = msg.content.split(" ")[0].slice(config.prefix.length).toLowerCase()
+var args = msg.content.split(" ").slice(1);
+
+console.log(command)
+console.log(args)
+
+if(msg.author.bot) {
+  return;
+}
+
+const ken = "ken";
+
+if (msg.content == ken) {
+  msg.channel.send(`hello im ken`)
+}
+
+  if(command == "ping") {
+    msg.channel.send(`Pinging...`).then(function(m) {
+      m.edit(`Pong! Took ${m.createdTimestamp - msg.createdTimestamp}ms`)
+    })
   }
 
-  console.log(`[${msg.createdAt}] by ${msg.author.tag} on ${msg.guild} in #${msg.channel.name}: ${msg.content}`)
+  if(command == "ken") {
+    msg.channel.send(`My name is Ken Jong Un, and I too am a homosexual.`)
+  }
+    console.log(`[${msg.createdAt}] by ${msg.author.tag} on ${msg.guild} in #${msg.channel.name}: ${msg.content}`)
 })
